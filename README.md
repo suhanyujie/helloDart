227f6dfdd32c95b1d829287008b9f313a281c529
suhanyujie <suhanyujie@qq.com>
2019-03-04 12:20:35 +0800

    today's commit done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # helloDart
+
+## 进度
+* http://dart.goodev.org/guides/language/language-tour#built-in-types%E5%86%85%E7%BD%AE%E7%9A%84%E7%B1%BB%E5%9E%8B
+
+## 参考资料
+* dart官方文档 http://dart.goodev.org/guides
--- bin/var/map.dart
+++ bin/var/map.dart
@@ -1,10 +1,22 @@
 void main(){
   var l1 = [11,12];
+  //声明一个map
   var m1 = {
     "name":"samuel",
+    "gender":"male",
+    'weight':130,
     l1:"something1"
   };
   print(m1);
+  //修改map中的值
+  m1['weight'] = 137;
+  print(m1);
+  //声明map的另一种方式
+  var m2 = new Map();
+  m2['name'] = 'Mark';
+  print(m2);
+
+
 }
 
 /**
