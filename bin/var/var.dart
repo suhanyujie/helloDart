54ea945733cf342ad3aeee32cca133477ebf89a1
suhanyujie <suhanyujie@qq.com>
2019-03-04 12:12:26 +0800

    commie some file

---
+++ bin/var/var.dart
@@ -0,0 +1,18 @@
+void main(){
+  var name="samuel";
+  int age = 18;
+  //打印 整形转字符串
+  print(name+"'s age is:"+age.toString());
+
+  //声明list变量
+  var l1 = [1,2,3];
+  print(l1);
+}
+
+/*
+## 变量的声明
+* 使用`var`关键字，可以声明不确定类型的变量
+* 也可以使用类型限定的方式声明确定类型的变量：`int age = 19;`
+
+
+ */
