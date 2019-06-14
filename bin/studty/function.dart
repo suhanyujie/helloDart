f415a011b6c575d89b415043358cca8ee9184cce
suhanyujie <suhanyujie@qq.com>
2019-06-14 18:59:16 +0800

    add some code

---
+++ bin/studty/function.dart
@@ -0,0 +1,33 @@
+void main() {
+  var f1 = testFunc;
+  print(f1);
+}
+
+void testFunc () {
+  print("func1");
+}
+
+
+
+/*
+## 方法
+* Dart 是一个真正的面向对象语言，方法也是对象并且具有一种类型 —— Function。
+* 因此，它可以赋值给变量：
+
+```dart
+void main() {
+  var f1 = testFunc;
+  print(f1);
+}
+
+void testFunc () {
+  print("func1");
+}
+```
+
+
+
+
+*/
+
+
---
+++ bin/studty/list.dart
@@ -0,0 +1,7 @@
+void main () {
+  List<String> l1 = ["who",'are','u'];
+  print(l1);
+  l1.add('u');
+  print(l1);
+
+}
